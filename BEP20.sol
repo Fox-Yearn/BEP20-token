255a3f8b9d7df704a1df2a0186752c964fca0c2b
Fox Yearn <61380204+Fox-Yearn@users.noreply.github.com>
2025-05-13 07:08:12 +0700

    BEP20.sol

--- BEP20.sol
+++ BEP20.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
 
+// BEP20 interface for standard token functionality
 interface IBEP20 {
     function totalSupply() external view returns (uint256);
     function decimals() external view returns (uint8);
@@ -17,16 +18,14 @@ interface IBEP20 {
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-contract Context {
+// Context for accessing msg.sender and msg.data
+abstract contract Context {
     function _msgSender() internal view returns (address) {
         return msg.sender;
     }
-
-    function _msgData() internal view returns (bytes memory) {
-        return msg.data;
-    }
 }
 
+// Ownable for ownership control
 contract Ownable is Context {
     address private _owner;
 
@@ -58,39 +57,33 @@ contract Ownable is Context {
     }
 }
 
-contract BEP20Token is Context, IBEP20, Ownable {
+// BEP20 token contract
+contract GullyDon is Context, IBEP20, Ownable {
     mapping(address => uint256) private _balances;
     mapping(address => mapping(address => uint256)) private _allowances;
 
     uint256 private _totalSupply;
-    uint8 private _decimals;
-    string private _symbol;
-    string private _name;
+    uint8 private constant _decimals = 18;
+    string private constant _name = "Gully Don";
+    string private constant _symbol = "Gully";
 
     constructor() {
-        _name = "Gully Don";
-        _symbol = "Gully";
-        _decimals = 18;
-        _totalSupply = 1_000_000_000_000_000_000_000_000_000; // 1 trillion tokens
+        // Initial supply: 1 trillion tokens (1e27 with 18 decimals)
+        _totalSupply = 1_000_000_000_000 * 10**_decimals;
         _balances[_msgSender()] = _totalSupply;
-
         emit Transfer(address(0), _msgSender(), _totalSupply);
     }
 
-    function getOwner() external view returns (address) {
-        return owner();
+    function name() external pure returns (string memory) {
+        return _name;
     }
 
-    function decimals() external view returns (uint8) {
-        return _decimals;
-    }
-
-    function symbol() external view returns (string memory) {
+    function symbol() external pure returns (string memory) {
         return _symbol;
     }
 
-    function name() external view returns (string memory) {
-        return _name;
+    function decimals() external pure returns (uint8) {
+        return _decimals;
     }
 
     function totalSupply() external view returns (uint256) {
@@ -99,6 +92,10 @@ contract BEP20Token is Context, IBEP20, Ownable {
 
     function balanceOf(address account) external view returns (uint256) {
         return _balances[account];
+    }
+
+    function getOwner() external view returns (address) {
+        return owner();
     }
 
     function transfer(address recipient, uint256 amount) external returns (bool) {
@@ -118,36 +115,36 @@ contract BEP20Token is Context, IBEP20, Ownable {
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool) {
         _transfer(sender, recipient, amount);
         uint256 currentAllowance = _allowances[sender][_msgSender()];
-        require(currentAllowance >= amount, "BEP20: transfer amount exceeds allowance");
+        require(currentAllowance >= amount, "GullyDon: transfer amount exceeds allowance");
         unchecked {
             _approve(sender, _msgSender(), currentAllowance - amount);
         }
         return true;
     }
 
-    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
+    function increaseAllowance(address spender, uint256 addedValue) external returns (bool) {
         _approve(_msgSender(), spender, _allowances[_msgSender()][spender] + addedValue);
         return true;
     }
 
-    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
+    function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool) {
         uint256 currentAllowance = _allowances[_msgSender()][spender];
-        require(currentAllowance >= subtractedValue, "BEP20: decreased allowance below zero");
+        require(currentAllowance >= subtractedValue, "GullyDon: decreased allowance below zero");
         unchecked {
             _approve(_msgSender(), spender, currentAllowance - subtractedValue);
         }
         return true;
     }
 
-    function mint(uint256 amount) public onlyOwner returns (bool) {
+    function mint(uint256 amount) external onlyOwner returns (bool) {
         _mint(_msgSender(), amount);
         return true;
     }
 
     function _transfer(address sender, address recipient, uint256 amount) internal {
-        require(sender != address(0), "BEP20: transfer from the zero address");
-        require(recipient != address(0), "BEP20: transfer to the zero address");
-        require(_balances[sender] >= amount, "BEP20: transfer amount exceeds balance");
+        require(sender != address(0), "GullyDon: transfer from the zero address");
+        require(recipient != address(0), "GullyDon: transfer to the zero address");
+        require(_balances[sender] >= amount, "GullyDon: transfer amount exceeds balance");
 
         unchecked {
             _balances[sender] -= amount;
@@ -157,7 +154,7 @@ contract BEP20Token is Context, IBEP20, Ownable {
     }
 
     function _mint(address account, uint256 amount) internal {
-        require(account != address(0), "BEP20: mint to the zero address");
+        require(account != address(0), "GullyDon: mint to the zero address");
 
         _totalSupply += amount;
         unchecked {
@@ -166,31 +163,11 @@ contract BEP20Token is Context, IBEP20, Ownable {
         emit Transfer(address(0), account, amount);
     }
 
-    function _burn(address account, uint256 amount) internal {
-        require(account != address(0), "BEP20: burn from the zero address");
-        require(_balances[account] >= amount, "BEP20: burn amount exceeds balance");
-
-        unchecked {
-            _balances[account] -= amount;
-            _totalSupply -= amount;
-        }
-        emit Transfer(account, address(0), amount);
-    }
-
     function _approve(address owner, address spender, uint256 amount) internal {
-        require(owner != address(0), "BEP20: approve from the zero address");
-        require(spender != address(0), "BEP20: approve to the zero address");
+        require(owner != address(0), "GullyDon: approve from the zero address");
+        require(spender != address(0), "GullyDon: approve to the zero address");
 
         _allowances[owner][spender] = amount;
         emit Approval(owner, spender, amount);
-    }
-
-    function _burnFrom(address account, uint256 amount) internal {
-        uint256 currentAllowance = _allowances[account][_msgSender()];
-        require(currentAllowance >= amount, "BEP20: burn amount exceeds allowance");
-        unchecked {
-            _approve(account, _msgSender(), currentAllowance - amount);
-        }
-        _burn(account, amount);
     }
 }
